c32adb9471aeec08fdc97e08d9a2e7c6217188fd
Yugapriya Shankar <shankary@uwindsor.ca>
2024-04-22 13:25:05 -0400

    reverted the previous change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,30 +17,30 @@ export const Navbar = () => {
     }
     return (
         //below is for single page reference. 300 status code in n/w tab
-        // <nav>
-        //     <img src={ logo } alt="Logo"/>
-        //     <NavLink style={ navLinkStyles } to='/LogoLookUpPage'>Logo Lookup</NavLink>  
-        //     <NavLink style={ navLinkStyles } to='/SubmitFeedback'>Submit Feedback</NavLink>
-        //     <NavLink style={ navLinkStyles } to='/DocumentatioDetails'>Check Documentation</NavLink>
-        //     <NavLink style={ navLinkStyles } to='/AboutUs'>About</NavLink>
-        //  </nav>
-        <ul>
-            <li>
-                <img src={ logo } alt="Logo"/>
-            </li>
-            <li>
-                <a href='/LogoLookUpPage' >Logo Lookup</a>
-            </li>
-            <li>
-                <a href='/SubmitFeedback' >Submit Feedback</a>
-            </li>
-            <li>
-                <a href='/DocumentatioDetails' >Check Documentation</a>
-            </li>
-            <li>
-                <a href='/AboutUs' >About</a>
-            </li>
-        </ul>
+        <nav>
+            <img src={ logo } alt="Logo"/>
+            <NavLink style={ navLinkStyles } to='/LogoLookUpPage'>Logo Lookup</NavLink>  
+            <NavLink style={ navLinkStyles } to='/SubmitFeedback'>Submit Feedback</NavLink>
+            <NavLink style={ navLinkStyles } to='/DocumentatioDetails'>Check Documentation</NavLink>
+            <NavLink style={ navLinkStyles } to='/AboutUs'>About</NavLink>
+         </nav>
+        // <ul>
+        //     <li>
+        //         <img src={ logo } alt="Logo"/>
+        //     </li>
+        //     <li>
+        //         <a href='/LogoLookUpPage' >Logo Lookup</a>
+        //     </li>
+        //     <li>
+        //         <a href='/SubmitFeedback' >Submit Feedback</a>
+        //     </li>
+        //     <li>
+        //         <a href='/DocumentatioDetails' >Check Documentation</a>
+        //     </li>
+        //     <li>
+        //         <a href='/AboutUs' >About</a>
+        //     </li>
+        // </ul>
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css'
-import './index.css'
-import LandingPage from './components/LandingPage'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import LandingPage from './components/LandingPage';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
